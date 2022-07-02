39fb728be1ae6f013f980861cc60646346d85863
Vatsal Verma <59110359+vatsal259@users.noreply.github.com>
2022-07-02 23:13:31 +0530

    Update CTA.js

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,7 +1,7 @@
 const CTA = () => {
     return (
         <div className={"CTA"}>
-            <a href="https://drive.google.com/file/d/1Z6f3_OHvHr4bc8nlrvPE1xPJ7CEi0tlO/view?usp=sharing" download className={"btn"}  target="_blank" rel="noreferrer">Resume</a>
+            <a href="https://drive.google.com/drive/folders/1L85dXbFHJc-6tEboZ1wMIplEg7MknA-q?usp=sharing" download className={"btn"}  target="_blank" rel="noreferrer">Resume</a>
             <a href={"#contact"} className={"btn btn-primary"}>Let's Talk</a>
         </div>
     )
