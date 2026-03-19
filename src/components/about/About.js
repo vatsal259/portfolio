786bb90e3331b1e46417c47567813e2db43bb5f9
Vatsal Verma <vatsalverma999@gmail.com>
2026-03-20 00:31:46 +0530

    UI fixes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -26,8 +26,8 @@ const About = () => {
                 </div>
                 <div className={'about__content'}>
                     <p className={"about_para"}>I'm a <strong>Software Developer</strong> passionate about building meaningful and impactful technology. I specialize in working with <strong>Microservices Architectures</strong>, love diving into <strong>AI/ML</strong>, and enjoy crafting clean, user-centered <strong>UI/UX</strong> experiences. My goal is always to solve real-world problems with thoughtful, well-structured code.</p>
-                    <p className={"about_para"}>When I’m not coding, you’ll find me riding my <strong>Royal Enfield Hunter</strong>, chasing open roads, quiet curves, and new perspectives. There’s something about the freedom of the ride  the rhythm of the engine, the wind in my face  that clears my head like nothing else.</p>
-                    <p className={"about_para"}>More often than not, I’ve got a camera slung over my shoulder, ready to capture whatever catches my eye  a bird in flight, a perfect patch of light, or a moment that feels too real to ignore.</p>
+                    <p className={"about_para"}>When I’m not coding, you’ll find me riding my <strong>Royal Enfield Hunter</strong>, chasing open roads, quiet curves, and new perspectives. There’s something about the freedom of the ride the steady rhythm of the engine and the wind in my face that clears my head like nothing else and provides a stark, refreshing contrast to a day spent behind the screen.</p>
+                    <p className={"about_para"}>More often than not, I’ve got a camera slung over my shoulder, ready to capture whatever catches my eye a bird in flight, a perfect patch of light, or the stark, minimalist silhouette of a single leafless tree that feels far too real to ignore and deserves to be preserved.</p>
                </div>
             </div>
         </section>
