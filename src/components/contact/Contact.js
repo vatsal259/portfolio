5d8a20ce5d2efb74e3eccdd723a43014ef247f80
Vatsal Verma <vatsalverma999@gmail.com>
2026-03-21 14:13:07 +0530

    UI fixes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,8 @@
-import React, {useRef} from 'react'
+import React from 'react'
 import './Contact.css'
 import {MdOutlineEmail} from "react-icons/md";
 
 const Contact = () => {
-
-    const form = useRef();
 
     return (
         <section id={"contact"}>
--- src/components/header/Socials.js
+++ src/components/header/Socials.js
@@ -7,14 +7,58 @@ import { RiTwitterXFill } from "react-icons/ri";
 
 
 function Socials() {
+    const socials = [
+        {
+            name: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/vatsalverma999/',
+            icon: <BsLinkedin size={'1rem'}/>
+        },
+        {
+            name: 'Instagram',
+            href: 'https://instagram.com/vatsalastav',
+            icon: <BsInstagram size={'1rem'}/>
+        },
+        {
+            name: 'Behance',
+            href: 'https://www.behance.net/vatsalverma',
+            icon: <ImBehance2 size={'1rem'}/>
+        },
+        {
+            name: 'GitHub',
+            href: 'https://github.com/vatsal259',
+            icon: <FaGithub size={'1rem'}/>
+        },
+        {
+            name: 'YouTube',
+            href: 'https://www.youtube.com/@walkinthecraziestway',
+            icon: <FaYoutube size={'1rem'}/>
+        },
+        {
+            name: 'X',
+            href: 'https://x.com/ellipsecircle',
+            icon: <RiTwitterXFill size={'1rem'}/>
+        }
+    ]
+
     return (
         <div className={"header__socials"}>
-            <a href={"https://www.linkedin.com/in/vatsalverma999/"} target={"_blank"} rel={"noreferrer"}><BsLinkedin size={"1.8rem"}/></a>
-            <a href={"https://instagram.com/vatsalastav"} target={"_blank"} rel="noreferrer"><BsInstagram size={"1.8rem"}/></a>
-            <a href={"https://www.behance.net/vatsalverma"} target={"_blank"} rel={"noreferrer"}><ImBehance2 size={"1.8rem"}/></a>
-            <a href={"https://github.com/vatsal259"} target={"_blank"} rel={"noreferrer"}><FaGithub size={"1.8rem"}/></a>
-            <a href={"https://www.youtube.com/@walkinthecraziestway"} target={"_blank"} rel="noreferrer"><FaYoutube size={"1.8rem"}/></a>
-            <a href={"https://x.com/ellipsecircle"} target={"_blank"} rel="noreferrer"><RiTwitterXFill size={"1.8rem"}/></a>
+            <div className={"header__socials-grid"}>
+                {socials.map(({name, href, icon}, index) => (
+                    <a
+                        key={name}
+                        href={href}
+                        target={"_blank"}
+                        rel={"noreferrer"}
+                        aria-label={`Open ${name} profile`}
+                        className={"header__social-card"}
+                        title={name}
+                        style={{'--i': index + 1}}
+                    >
+                        <span className={"header__social-icon"}>{icon}</span>
+                        <span className={"header__social-tooltip"}>{name}</span>
+                    </a>
+                ))}
+            </div>
         </div>
     );
 }
